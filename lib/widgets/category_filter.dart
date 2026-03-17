25b0f5f98755c038c9abd1c0f07459c3c2defa53
Justin <sunjiaqing216@126.com>
2026-03-17 15:16:24 +0800

    上部导航栏胶囊

--- lib/widgets/category_filter.dart
+++ lib/widgets/category_filter.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import '../models/pose_category.dart';
 
@@ -14,68 +13,69 @@ class CategoryFilter extends StatelessWidget {
     required this.onCategorySelected,
   }) : super(key: key);
 
+  Widget _buildCategoryChip(PoseCategory category, bool isSelected) {
+    return GestureDetector(
+      onTap: () => onCategorySelected(isSelected ? null : category.id),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
+        decoration: BoxDecoration(
+          color: isSelected ? null : Colors.white.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            color: isSelected
+                ? Colors.white.withOpacity(0.9)
+                : Colors.white.withOpacity(0.6),
+            width: 1.5,
+          ),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: const Color(0xFF6C63FF).withOpacity(0.2),
+                    blurRadius: 16,
+                    offset: const Offset(0, 4),
+                  ),
+                  BoxShadow(
+                    color: Colors.white.withOpacity(0.4),
+                    blurRadius: 8,
+                    spreadRadius: 1,
+                  ),
+                ]
+              : [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 8,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+        ),
+        child: Text(
+          '${category.icon}  ${category.name}',
+          style: TextStyle(
+            color: isSelected ? const Color(0xFF6C63FF) : const Color(0xFF555555),
+            fontWeight: isSelected ? FontWeight.w600 : FontWeight.w500,
+            fontSize: 13,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
-        separatorBuilder: (context, index) => const SizedBox(width: 12),
-        itemBuilder: (context, index) {
-          final category = categories[index];
-          bool isSelected = selectedCategoryId == category.id;
-
-          return GestureDetector(
-            onTap: () {
-              onCategorySelected(isSelected ? null : category.id);
-            },
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
-              decoration: BoxDecoration(
-                color: isSelected ? null : Colors.white.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(
-                  color: isSelected
-                      ? Colors.white.withOpacity(0.9)
-                      : Colors.white.withOpacity(0.6),
-                  width: 1.5,
-                ),
-                boxShadow: isSelected
-                    ? [
-                        BoxShadow(
-                          color: const Color(0xFF6C63FF).withOpacity(0.2),
-                          blurRadius: 16,
-                          offset: const Offset(0, 4),
-                        ),
-                        BoxShadow(
-                          color: Colors.white.withOpacity(0.4),
-                          blurRadius: 8,
-                          spreadRadius: 1,
-                        ),
-                      ]
-                    : [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.05),
-                          blurRadius: 8,
-                          offset: const Offset(0, 2),
-                        ),
-                      ],
-              ),
-              child: Center(
-                child: Text(
-                  '${category.icon}  ${category.name}',
-                  style: TextStyle(
-                    color: isSelected ? const Color(0xFF6C63FF) : const Color(0xFF555555),
-                    fontWeight: isSelected ? FontWeight.w600 : FontWeight.w500,
-                    fontSize: 13,
-                  ),
-                ),
-              ),
+      child: Row(
+        children: [
+          for (int i = 0; i < categories.length; i++) ...[
+            if (i > 0) const SizedBox(width: 12),
+            _buildCategoryChip(
+              categories[i],
+              selectedCategoryId == categories[i].id,
             ),
-          );
-        },
+          ],
+        ],
       ),
     );
   }
